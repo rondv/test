876548afbef83d702fa162e8960f68709b673d41
Tom Grennan <tgrennan@platinasystems.com>
2019-05-29 15:49:17 -0700

    copyright 2019
    Signed-off-by: Tom Grennan <tgrennan@platinasystems.com>

--- license.go
+++ license.go
@@ -1,7 +1,7 @@
 package test
 
 const License = `
-Copyright © 2015-2018 Platina Systems, Inc. All rights reserved.
+Copyright © 2015-2019 Platina Systems, Inc. All rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the terms of the GNU General Public License as published by the
