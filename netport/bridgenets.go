b05ca87e956801b2760e929a610e5dc9eba4bd87
Ron Taylor <rtaylor@platinasystems.com>
2020-03-05 14:43:34 -0800

    add tests for untagged bridge members
    Signed-off-by: Ron Taylor <rtaylor@platinasystems.com>

--- netport/bridgenets.go
+++ netport/bridgenets.go
@@ -139,3 +139,121 @@ var BridgeNets2 = NetDevs{
 		Remotes: []string{"10.2.0.1", "10.1.0.2", "10.1.0.20"},
 	},
 }
+
+var BridgeNets1u = NetDevs{
+	{
+		Netns:   "h1",
+		NetPort: "net0port0",
+		Ifa:     "10.1.0.2/24",
+		Routes: []Route{
+			{"10.2.0.0/24", "10.1.0.1"},
+		},
+		Remotes: []string{"10.1.0.1", "10.2.0.1", "10.2.0.2"},
+	},
+
+	{
+		Netns:         "r",
+		IsBridge:      true,
+		BridgeIfindex: BridgeIndexBase + 0,
+		Ifname:        "tb1",
+		BridgeMac:     "00:00:01:b1:b1:b1",
+		Ifa:           "10.1.0.1/24",
+		Remotes:       []string{"10.1.0.2", "10.2.0.2"},
+	},
+	{
+		Netns:   "r",
+		NetPort: "net0port1",
+		Upper:   "tb1",
+	},
+	{
+		Netns:         "r",
+		IsBridge:      true,
+		BridgeIfindex: BridgeIndexBase + 1,
+		Ifname:        "tb3",
+		BridgeMac:     "00:00:01:b3:b3:b3",
+		Ifa:           "10.2.0.1/24",
+		Remotes:       []string{"10.1.0.2", "10.2.0.2"},
+	},
+	{
+		Netns:   "r",
+		NetPort: "net1port1",
+		Upper:   "tb3",
+	},
+
+	{
+		Netns:   "h2",
+		NetPort: "net1port0",
+		Ifa:     "10.2.0.2/24",
+		Routes: []Route{
+			{"10.1.0.0/24", "10.2.0.1"},
+		},
+		Remotes: []string{"10.2.0.1", "10.1.0.1", "10.1.0.2"},
+	},
+}
+
+var BridgeNets2u = NetDevs{
+	{
+		Netns:   "h1",
+		NetPort: "net0port1",
+		Ifa:     "10.1.0.2/24",
+		Routes: []Route{
+			{"10.2.0.0/24", "10.1.0.1"},
+		},
+		Remotes: []string{"10.1.0.1", "10.2.0.1", "10.2.0.2"},
+	},
+
+	// L2 bridge
+	{
+		Netns:         "b1",
+		IsBridge:      true,
+		BridgeIfindex: BridgeIndexBase + 0,
+		Ifname:        "tb1",
+		BridgeMac:     "00:00:02:b1:b1:b1",
+		Ifa:           "10.1.0.20/24",
+		Routes: []Route{
+			{"default", "10.1.0.1"},
+		},
+	},
+	{
+		Netns:   "b1",
+		NetPort: "net0port0",
+		Upper:   "tb1",
+	},
+	{
+		Netns:   "b1",
+		NetPort: "net1port0",
+		Upper:   "tb1",
+	},
+
+	// L3 bridge
+	{
+		Netns:         "r2",
+		IsBridge:      true,
+		BridgeIfindex: BridgeIndexBase + 1,
+		Ifname:        "tb2",
+		BridgeMac:     "00:00:02:b2:b2:b2",
+		Ifa:           "10.1.0.1/24",
+		Remotes:       []string{"10.1.0.2", "10.2.0.2"},
+	},
+	{
+		Netns:   "r2",
+		NetPort: "net1port1",
+		Upper:   "tb2",
+	},
+	{
+		Netns:   "r2",
+		NetPort: "net2port0",
+		Ifa:     "10.2.0.1/24",
+		Remotes: []string{"10.1.0.2", "10.2.0.2"},
+	},
+
+	{
+		Netns:   "h3",
+		NetPort: "net2port1",
+		Ifa:     "10.2.0.2/24",
+		Routes: []Route{
+			{"10.1.0.0/24", "10.2.0.1"},
+		},
+		Remotes: []string{"10.2.0.1", "10.1.0.2", "10.1.0.20"},
+	},
+}
